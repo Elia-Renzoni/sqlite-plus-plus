82dbd7c6061d82ff2d855320aac2f93929263bde
Elia-Renzoni <elia.renzoni03@gmail.com>
2025-10-28 17:05:58 +0100

    fix(cluster): add term management for leader election
    Signed-off-by: Elia-Renzoni <elia.renzoni03@gmail.com>

--- cluster/leader_election.py
+++ cluster/leader_election.py
@@ -1,4 +1,24 @@
 import manager as coord
 
+class ElectionTerm:
+    def __init__(self):
+        self.term = 0
+    
+    def new_term(self):
+        self.term += 1
+    
+    def compare_terms(self, remote_leader_term):
+        if remote_leader_term < self.term:
+            # split brain occured
+            raise "Split Brain! Notice the old leader"
+        
+        # merge the term 
+        if remote_leader_term > self.term:
+            self.term = remote_leader_term
+
+
 def start_leader_election():
+    pass
+
+async def wait_for_heartbeat():
     pass
