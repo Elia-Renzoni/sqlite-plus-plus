205ecf4290e1d9840c61c46ce2d99e32dde24d99
Elia-Renzoni <elia.renzoni03@gmail.com>
2025-10-28 17:17:14 +0100

    fix(cluster): delete method and add proper alternative
    Signed-off-by: Elia-Renzoni <elia.renzoni03@gmail.com>

--- cluster/leader_election.py
+++ cluster/leader_election.py
@@ -16,12 +16,18 @@ class ElectionTerm:
         if remote_leader_term > self.term:
             self.term = remote_leader_term
 
-
 term_manager = ElectionTerm()
+leader_is_me = None
 
+def try_fetch_leader(tcpAddress):
+    leader = coord.get("leader")
+    if leader is not None:
+        return
+    propose_vote()
 
-def start_leader_election():
-    pass
+def propose_vote(tcp_address):
+    success = coord.set("leader", tcp_address)
+    leader_is_me = success
 
 async def wait_for_heartbeat():
     pass
